c91c19656da869154d91a5aebfcd67c90035b0ed
JPLogics <joshua.pope-lewis@outlook.com>
2017-06-06 10:52:05 +0100

    Update startup.lua
    Checks if system has been setup or if the main executable is missing.

--- startup.lua
+++ startup.lua
@@ -1,1 +1,18 @@
 -- Installer
+term.clear()
+term.setCursorPos(1,1)
+print("JPLogic ROC")
+print("Inital Boot?")
+if fs.exists("Data/config") and fs.exists("startup-main") then
+print("System is good")
+print("All files exist")
+print("Config file set")
+print("Launching program...")
+sleep(0.1)
+shell.run("startup-main")
+else
+print("Config does not exist/missing file")
+print("Launching Installer...")
+sleep(0.5)
+shell.run("installer")
+end
