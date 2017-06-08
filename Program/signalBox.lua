38a1fa5fb7602abdcbcf9d7e88c2b4da7c825c60
JPLogics <joshua.pope-lewis@outlook.com>
2017-06-08 11:01:49 +0100

    Updated signalBox.lua
    Added core code for searching Timetables.
    --Untested, may not work 100%.

--- Program/signalBox.lua
+++ Program/signalBox.lua
@@ -1,5 +1,5 @@
 --Signalbox Executable
---Vars
+--Vars - These will be ref from Config File
 local title = "Signal Box Server"
 local signalBox = "JPLogic Central"
 local locationName = "Platform 1"
@@ -7,21 +7,38 @@ local signalBoxID = 6
 local data = {}
 local trainInfo = {}
 local response = false
-
+local adminpassword = "test"
 --Startup
 local SignalCableSide = "top"
 local rednetSide = "back"
 local PointCableSide = "bottom"
 rednet.open(rednetSide)
+--Runs
 os.loadAPI("Data/background")
 shell.run("Data/background")
 
 --Functions
 local function waitMessage()
   id, message, proto = rednet.receive()
-  if proto == "(something)" then
-    -- Do stuff
-  elseif proto == "(something else)" then
+  if proto == "stationStop" then
+    shell.run("Data/timetables")
+    data = textutils.unserialize(message)
+    RLocation = data.Location
+    UID = data.UID
+    sentTime = data.Time
+    for i,v in pairs(UID) do
+      if i == signalBox then
+        if table[UID].signalBox.Location == RLocation then
+          ttLocation = table[UID].signalBox.Location
+          ttArr = table[UID].signalBox.Location.Data.Arr
+          ttDep = table[UID].signalBox.Location.Data.Dep
+          ttExit = table[UID].signalBox.Location.Data.Exit
+        else
+        end
+      else
+      end
+    end
+  elseif proto == "Checkpoint" then
     -- Do stuff
   else
     sleep(0.2)
@@ -30,8 +47,20 @@ end
 
 local function waitKey()
   event, key = os.pullEvent("char")
-  if key == keys.(a key) then
-    -- Do stuff
+  if key == keys.t then
+    print("Key: T, has been pressed: Terminating Program")
+    print("Confirm you want to Terminate "..signalBox.." "..title.."?")
+    confirm = read()
+    if confirm == "" then
+      print("Termination request cancelled!")
+    elseif confirm == adminpassword then
+      print("Password: Correct - Terminating")
+      sleep(0.5)
+      break
+    else
+      print("Invalid entry!")
+      print("Termination request cancelled!")
+    end
   elseif key == keys.(a key) then
     -- Do stuff
   else
